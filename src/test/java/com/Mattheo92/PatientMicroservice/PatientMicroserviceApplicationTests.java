a3eab03de93a880d15d8a4b6cc8e034a9174a7a6
Mattheo992 <stepniak.matt@gmail.com>
2024-07-22 16:33:18 +0200

    Initial commit

---
+++ src/test/java/com/Mattheo92/PatientMicroservice/PatientMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Mattheo92.PatientMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
